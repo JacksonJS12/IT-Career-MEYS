0cb5f0985f0b8c6acf57578f8572dd4181acc136
Deniz <d.ivan0v.6100@gmail.com>
2022-11-15 21:29:33 +0200

    added exercises labs

---
+++ 01.Introduction to programming/03.Checks (conditional constructions)/P01.Excellent_Result/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01.Excellent_Result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
