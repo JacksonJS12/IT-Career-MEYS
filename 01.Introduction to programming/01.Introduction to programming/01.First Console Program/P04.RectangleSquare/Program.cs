7ca77347b3a60f04e9622245a472a2356b364022
Deniz <d.ivan0v.6100@gmail.com>
2022-11-15 17:04:04 +0200

    First lesson completed

---
+++ 01.Introduction to programming/01.Introduction to programming/01.First Console Program/P04.RectangleSquare/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P04.RectangleSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numA = decimal.Parse(Console.ReadLine());
+            var numB = decimal.Parse(Console.ReadLine());
+
+            var area = numA * numB;
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ 01.Introduction to programming/01.Introduction to programming/01.First Console Program/P05.SquareOfNStars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P05.SquareOfNStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < inputNum; i++)
+            {
+                if(i == 0 ||
+                    i == inputNum - 1)
+                {
+                    for (int k = 0; k < inputNum; k++)
+                    {
+                        Console.Write('*');
+                    }
+                }
+                else
+                {
+                    for (int k = 0; k < inputNum; k++)
+                    {
+                        if(k == 0 ||
+                            k == inputNum - 1)
+                        {
+                            Console.Write('*');
+                            continue;
+                        }
+                        Console.Write(' ');
+
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
