b838fa0deba65325fe4cb252981297fc03b6db8c
Deniz <d.ivan0v.6100@gmail.com>
2022-11-17 13:17:44 +0200

    fix

--- 01.Introduction to programming/03.Checks (conditional constructions)/P16.NumberToWords_100/Program.cs
+++ 01.Introduction to programming/03.Checks (conditional constructions)/P16.NumberToWords_100/Program.cs
@@ -7,12 +7,42 @@ namespace P16.NumberToWords_100
         static void Main(string[] args)
         {
             var inputNum = int.Parse(Console.ReadLine());
+            string result = string.Empty;
 
-            if(inputNum >  ) 
-            { 
-
+            if (inputNum < 10 &&
+                inputNum >= 0)
+            {
+                result += From1To9(inputNum);
             }
-            var result = "";
+            else if (inputNum < 100)
+            {
+                int firstDigit = inputNum / 10;
+                int secondDigit = inputNum % 10;
+
+                result += From10To90(firstDigit) +
+                          " " +
+                          From1To9(secondDigit);
+
+                if (secondDigit == 0)
+                {
+                    result = From10To90(firstDigit);
+                }
+            }
+            else if (inputNum == 100)
+            {
+                result = "one hundred";
+            }
+            else
+            {
+                result = "too big number!";
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static string From1To9(int inputNum)
+        {
+            var result = string.Empty;
             switch (inputNum)
             {
                 case 0:
@@ -45,12 +75,46 @@ namespace P16.NumberToWords_100
                 case 9:
                     result = "nine";
                     break;
-                default:
-                    result = "number too big";
+            }
+
+            return result;
+        }
+        private static string From10To90(int inputNum)
+        {
+            var result = string.Empty;
+            switch (inputNum)
+            {
+                case 1:
+                    result = "ten";
+                    break;
+                case 2:
+                    result = "twenty";
+                    break;
+                case 3:
+                    result = "thirty";
+                    break;
+                case 4:
+                    result = "forty";
+                    break;
+                case 5:
+                    result = "fifty";
+                    break;
+                case 6:
+                    result = "sixty";
+                    break;
+                case 7:
+                    result = "seventy";
+                    break;
+                case 8:
+                    result = "eighty";
+                    break;
+                case 9:
+                    result = "ninety";
                     break;
             }
 
-            Console.WriteLine(result);
+            return result;
         }
     }
 }
+
