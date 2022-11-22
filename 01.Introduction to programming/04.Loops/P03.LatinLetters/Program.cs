b680eecccd88105b127ccd850f96e93e9ec713db
Deniz <d.ivan0v.6100@gmail.com>
2022-11-23 00:47:56 +0200

    some exercises

---
+++ 01.Introduction to programming/04.Loops/P03.LatinLetters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P03.LatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //in decimal numeral in ASCII table 'a' is 97 and 'z' is 122
+            Console.Write("Latin alphabet: ");
+            for (int i = 97; i <= 122; i++)
+            {
+                char ch = (char)i;
+                Console.Write(ch + " ");
+            }
+        }
+    }
+}
---
+++ 01.Introduction to programming/04.Loops/P06.MinNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P06.MinNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long smallestNum = long.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                long currentNum = int.Parse(Console.ReadLine());
+                smallestNum = currentNum < smallestNum ? currentNum : smallestNum;
+            }
+            Console.WriteLine(smallestNum);
+        }
+    }
+}
