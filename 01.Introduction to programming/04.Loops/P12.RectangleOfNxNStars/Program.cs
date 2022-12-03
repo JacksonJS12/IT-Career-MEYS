c0074f3c6cd6e83e7bd103f4fedf6d0eaee9ee79
Deniz <d.ivan0v.6100@gmail.com>
2022-12-04 00:17:33 +0200

    commit

---
+++ 01.Introduction to programming/04.Loops/P12.RectangleOfNxNStars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P12.RectangleOfNxNStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
