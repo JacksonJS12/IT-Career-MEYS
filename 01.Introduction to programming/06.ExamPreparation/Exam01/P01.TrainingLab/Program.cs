61ece89b5a3365482faab52c4ed15c9343598804
Deniz <d.ivan0v.6100@gmail.com>
2022-12-04 03:04:41 +0200

    exam prep exercises

---
+++ 01.Introduction to programming/06.ExamPreparation/Exam01/P01.TrainingLab/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P01.TrainingLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            double rows = Math.Truncate((width * 100 - 100) / 70);
+            double cols = Math.Truncate(height * 100 / 120);
+
+            double ans = rows * cols - 3;
+            Console.WriteLine(ans);
+        }
+    }
+}
