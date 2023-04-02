d18e1246e627dd33103ae73ccbfe9394d7d951f9
Deniz <d.ivan0v.6100@gmail.com>
2023-04-02 23:47:58 +0300

    exercise and a half

---
+++ 02.Programming/Exam Preparation/Exam 04.09.2017/Exam04.09.2017/P01.Resurrection/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P01.Resurrection;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int lengthBody = int.Parse(Console.ReadLine());
+            double widthBody = double.Parse(Console.ReadLine());
+            int lengthEachWing = int.Parse(Console.ReadLine());
+
+            double totalYears = Math.Pow(lengthBody, 2) * (widthBody + 2 * lengthEachWing);
+            Console.WriteLine(totalYears);
+        }
+    }
+}
