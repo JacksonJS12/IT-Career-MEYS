5fd93cf8e3c7c52d73d802ac0e4443933021b26d
Deniz <d.ivan0v.6100@gmail.com>
2023-05-18 15:48:09 +0300

    exercises

---
+++ 03.Introduction to OOP/Encapsulation - Access Modifiers/P01.SalaryIncrease/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace P01.SalaryIncrease
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private double salary;
+
+        public Person(string firstName,string lastName, int age, double salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            private set { this.firstName = value; }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            private set { this.lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            private set { this.age = value; }
+        }
+        public double Salary
+        {
+            get { return this.salary; }
+            set { this.salary = value; }
+        }
+
+        public void IncreasedSalary(double percent)
+        {
+            if (this.age > 30)
+            {
+                this.Salary += this.Salary * percent / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percent / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} get {this.Salary:F2} leva";
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), double.Parse(cmdArgs[3]));
+                persons.Add(person);
+            }
+
+            var bonus = double.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreasedSalary(bonus));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
