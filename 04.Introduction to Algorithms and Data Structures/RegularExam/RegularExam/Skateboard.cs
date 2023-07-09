c3052a98b24acc3b113653572265da9735f88872
Deniz Ivanov <d.ivan0v.6100@gmail.com>
2023-07-09 11:47:12 +0300

    90/100

---
+++ 04.Introduction to Algorithms and Data Structures/RegularExam/RegularExam/Skateboard.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    public class Skateboard
+    {
+        private string model;
+        private double price;
+
+        public Skateboard(string model, double price)
+        {
+            this.Model = model;
+            this.Price = price;
+        }
+        public string Model
+        {
+            get
+            {
+               return this.model;
+            }
+            set
+            {
+                //if (!(Regex.IsMatch(value, @"^[a-zA-Z]+$")))
+                //{
+                //    throw new ArgumentException("Wrong name!");
+                //}
+                this.model = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Skateboard {this.Model} costs {this.Price:f2} lv.";
+        }
+
+    }
+}
